ae4be5152f4ed12596e99949df76808d1209db9c
leo graziani <graziani.gothe.leo@gmail.com>
2024-04-24 22:35:53 +0200

    randomized bot ship placement

---
+++ src/bot.rs
@@ -0,0 +1,75 @@
+use rand::Rng;
+use crate::Game;
+
+pub struct Bot {
+    pub game: Game,
+    hits: Vec<(usize, usize)>,
+    last_hit: Option<(usize, usize)>,
+    searching: bool,
+    turn: u8
+}
+
+impl Bot {
+    pub fn new() -> Self {
+        Bot {
+            game: Game::new(),
+            hits: Vec::new(),
+            last_hit: None,
+            searching: true,
+            turn: 0
+        }
+    }
+
+    pub fn track_hit(&mut self, row: usize, col: usize) {
+        self.hits.push((row, col));
+        self.last_hit = Some((row, col));
+    }
+
+    pub fn place_bot_ship(&mut self) {
+        let sizes: Vec<i32> = vec![5, 4, 3, 3];
+        for size in sizes {
+            loop {
+                let mut ship_size = size.clone();
+                let position = self.get_position(ship_size);
+                let placed: bool = self.game.place_pos(position, &mut ship_size, false);
+                if placed {
+                    break
+                }
+            }
+        }
+    }
+
+    pub fn get_position(&mut self, size: i32) -> Vec<usize> {
+        let mut rng = rand::thread_rng();
+        let max: usize = 10 - size as usize;
+        let horizontal: bool = rng.gen();
+        if horizontal {
+            let x: usize = rng.gen_range(0..10);
+            let y1: usize = rng.gen_range(0..max);
+            let y2: usize = y1 + size as usize - 1;
+            let position: Vec<usize> =  vec![x, y1, x, y2];
+            position
+        }
+        else {
+            let y: usize = rng.gen_range(0..10);
+            let x1: usize = rng.gen_range(0..max);
+            let x2: usize = x1 + size as usize - 1;
+            let position: Vec<usize> =  vec![x1, y, x2, y];
+            position
+        }
+    }
+
+    pub fn shoot(&mut self, player: &mut Game) {
+        let mut rng = rand::thread_rng();
+        let x: usize = rng.gen_range(0..10);
+        let y: usize = rng.gen_range(0..10);
+        self.game.take_shot_from_coord(&mut player.ships, x, y);
+        self.track_hit(y, x)
+    }
+
+    pub fn bot_turn(&mut self, player: &mut Game) -> bool {
+        self.shoot(player);
+        self.turn += 1;
+        player.check_game_lost()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,24 @@
-use rand::Rng;
 mod game_logic;
 use game_logic::Game;
+mod bot;
+use bot::Bot;
 
-
-fn place_bot_ship(bot_game: &mut Game) {
-    let carrier: Vec<usize> = vec![0, 1, 0, 5];
-    bot_game.place_pos(carrier, &mut 5);
-    let destroyer: Vec<usize> = vec![4, 3, 7, 3];
-    bot_game.place_pos(destroyer, &mut 4);
-    let cruiser: Vec<usize> = vec![9, 3, 9, 5];
-    bot_game.place_pos(cruiser, &mut 3);
-    let submarine: Vec<usize> = vec![0, 7, 0, 9];
-    bot_game.place_pos(submarine, &mut 3);
-}
-
-fn bot_turn(bot: &mut Game, player: &mut Game) -> bool {
-
-    let mut rng = rand::thread_rng();
-    let x: usize = rng.gen_range(0..10);
-    let y: usize = rng.gen_range(0..10);
-    bot.take_shot_from_coord(&mut player.ships, x, y);
-    let state: bool = player.check_game_lost();
-    state
-}
 
 fn main() {
     std::process::Command::new("clear").status().unwrap();
     let mut player = Game::new();
-    let mut bot = Game::new();
-
-    place_bot_ship(&mut bot);
+    let mut bot = Bot::new();
+    bot.place_bot_ship();
+    bot.game.display_both();
     player.place_ships();
 
     loop {
-        let state = player.player_turn(&mut bot);
+        let state = player.player_turn(&mut bot.game);
         if state {
             println!("You won!");
             break;
         }
-        let state = bot_turn(&mut bot, &mut player);
+        let state = bot.bot_turn(&mut player);
         if state {
             println!("You lost!");
             break;
